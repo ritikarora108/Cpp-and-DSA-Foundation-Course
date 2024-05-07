7cbbbcafd8a398afe11a51dc5051f9dea9f037a5
Ritik Arora <aroraritik300@gmail.com>
2024-05-07 19:45:31 +0530

    All pair shortest path algo

---
+++ 18) Graph/Floydd_Warshall/Floyyd_Warshall.cpp
@@ -0,0 +1,41 @@
+// Multisource / All pairs shortest path algorithm -> Floyyd Warshall
+// (detect negative cycles when cost[node][node]<0)
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    int edges;
+    cin >> edges;
+    vector<vector<int>> dist(n + 1, vector<int>(n + 1, 1e9));
+    while (edges--)
+    {
+        int src, dest, wt;
+        cin >> src >> dest >> wt;
+
+        dist[src][dest] = wt;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        dist[i][i] = 0;
+    }
+    for (int vertex = 0; vertex < n; vertex++)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                dist[i][j] = min(dist[i][j], dist[i][vertex] + dist[vertex][j]);
+            }
+        }
+    }
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << dist[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
